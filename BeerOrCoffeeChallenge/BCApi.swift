31ecbd71a0fe13cad0288169a9c9a315e53b03be
RoberthSiqueira <roberth.siqueira@gmail.com>
2016-06-23 22:49:01 -0300

    Create model Api

---
+++ BeerOrCoffeeChallenge/BCApi.swift
@@ -0,0 +1,16 @@
+//
+//  BCApi.swift
+//  BeerOrCoffeeChallenge
+//
+//  Created by Roberth Siqueira on 23/06/16.
+//  Copyright © 2016 Roberth Siqueira. All rights reserved.
+//
+
+import Foundation
+
+class Api {
+    let key: String = "AIzaSyBJT7GKGh5nNCC-HnRBI5dEJJmg3iGM4Mo"
+    func returnKey() -> String {
+        return key
+    }
+}
